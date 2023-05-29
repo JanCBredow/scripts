30c552e01f3c489a76a2957259bb114f4c8f7d1c
Jan Christopher Bredow <38568028+JanCBredow@users.noreply.github.com>
2023-05-29 21:40:53 +0200

    Update aliases.sh
    v2

--- aliases.sh
+++ aliases.sh
@@ -4,14 +4,14 @@ sudo tee -a ~/.bashrc > /dev/null <<EOT
 alias latest-release='sudo do-release-upgrade'
 alias ls='ls --color=auto -CalF'
 alias grep='grep --color=auto'
-alias traffic='vnstat'
-alias update='sudo apt update && sudo apt upgrade'
+alias traffic='vnstat -l'
+alias ports='sudo netstat -tulpn | grep LISTEN'
+alias portsNoSudo='netstat -tulpn | grep LISTEN'
+alias clean='sudo apt autoclean && sudo apt autoremove'
+alias update='sudo apt update && sudo apt upgrade && clean'
 alias back='cd ..'
 alias ups='cd -'
-alias df='df -h'
-alias du='du -h'
+alias space='df -h'
 EOT
 
-source ~/.bashrc
-
-echo "You are ready to go!"
+echo "Use \"source ~/.bashrc\" to activate or re-open the console."
